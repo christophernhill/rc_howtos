af70467be1180afb87d418e83a4fc5b1c12d2cbe
christophernhill <cnh@mit.edu>
2018-02-16 07:46:02 -0500

    Initial comments

---
+++ examples/create-singularity-c7-python-image/setup-vagrant-builder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# We need a Linux system with root access to create a Singulairy image that has all the needed
+# software preinstalled. If you don't already have root on a Linux system then the easiest
+# approach is to create a virtual machine running Linux just for creating the Singularity image. 
+# A quick way to do this is using "vagrant" and "virtual box". These are tools that are
+# avialble in both Windows and MacOS environments. 
+#
+# This script assumes that VirtualBox (https://www.virtualbox.org) and Vagrant (https://www.vagrantup.com)
+# are already installed on your system.
