c76062d82876c3ecba59439c9f5ceb30d8e45330
christophernhill <cnh@mit.edu>
2018-02-17 15:05:57 -0500

    Update setup-vagrant-builder.sh

--- examples/create-singularity-c7-python-image/setup-vagrant-builder.sh
+++ examples/create-singularity-c7-python-image/setup-vagrant-builder.sh
@@ -1,4 +1,4 @@
-R#!/bin/bash
+#!/bin/bash
 #
 # We need a Linux system with root access to create a Singulairy image that has all the needed
 # software preinstalled. If you don't already have root on a Linux system then the easiest
