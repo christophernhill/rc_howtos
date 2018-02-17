826653deb0dafc037ddf13338e5cba77e072097e
christophernhill <cnh@mit.edu>
2018-02-17 15:05:46 -0500

    Update setup-vagrant-builder.sh

--- examples/create-singularity-c7-python-image/setup-vagrant-builder.sh
+++ examples/create-singularity-c7-python-image/setup-vagrant-builder.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+R#!/bin/bash
 #
 # We need a Linux system with root access to create a Singulairy image that has all the needed
 # software preinstalled. If you don't already have root on a Linux system then the easiest
@@ -19,21 +19,33 @@ CURDIR=`pwd`
 
 # 1. Create config file to ensure VM has reasonable memory
 #    (default Vagrant is 512MB, which is too small for some Python 
-#    (package compile stages.
-cat > Customfile <<'EOFA'
- config.vm.provider "virtualbox" do |v|
-  v.memory = 4096
-  v.cpus = 4
- end
+#    (package compile stages. Here we create commands to edit config 
+#    (for 4GB and 4 virtual cpu cores.
+cat > efile <<'EOFA'
+/  # config.vm.provider "virtualbox" do |vb|/
+a
+  config.vm.provider "virtualbox" do |v|
+   v.memory = 4096
+   v.cpus = 4
+  end
+.
+w
+q
 EOFA
 
 # 2. Create a vagrant image description for an Ubuntu machine
+#    and edit description to have config customizations that are
+#    needed.
 # Ubuntu seems to handle "Guest Additions", that allow host file
 # system to map into VM, well. This is a bit broken in CentOS 
 # (see https://github.com/CentOS/sig-cloud-instance-build/issues/107 )
 vagrant init --force ubuntu/trusty64
+cat efile | ed Vagrantfile
 
-# 3. Provide instructions for next step
+# 3. Provide instructions for next step.
+echo ' '
+echo '###################################'
 echo 'Creation of vagrant VM config done'
 echo 'Setup is in directory '${CURDIR}
 echo "To start VM \" cd ${CURDIR}; vagrant up\""
+echo '###################################'
