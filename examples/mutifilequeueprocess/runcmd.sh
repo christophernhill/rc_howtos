784d68fc64e81962dc6f95ab580e01b636e5a869
christophernhill <cnh@mit.edu>
2018-02-19 22:54:27 -0500

    Update runcmd.sh

--- examples/mutifilequeueprocess/runcmd.sh
+++ examples/mutifilequeueprocess/runcmd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+fn=$1
 pwd
 sdirs="-B /nfs/cnhlab003/cnh -B /nobackup1b/users/cnh -B /pool001/cnh"
-singularity exec ${sdirs} centos7ro.img python -s func.py
+singularity exec ${sdirs} centos7ro.img python -s func.py ${fn}
